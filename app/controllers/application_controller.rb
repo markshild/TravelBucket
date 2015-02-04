648c48dbdac221ab4d6b1ef92ad51766527d0a7e
Mark Shildkret <markshild@gmail.com>
2015-02-04 16:01:24 -0500

    Adds session table and application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,31 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :signed_in?
+
+  private
+  def current_user
+    @current_user ||= session[:session_token] ? User.find(Session.find_by_session_token(session[:session_token]).user_id) : nil
+  end
+
+  def signed_in?
+    !!current_user
+  end
+
+  def sign_in(user)
+    @current_user = user
+    new_token = SecureRandom.urlsafe_base64(16)
+    session[:session_token] = new_token
+    Session.create(user_id: user.id, session_token: new_token)
+  end
+
+  def sign_out
+    sess = Session.find_by_session_token(session[:session_token])
+    sess.destroy
+    session[:session_token] = nil
+  end
+
+  def require_signed_in!
+    redirect_to new_session_url unless signed_in?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204204359) do
+ActiveRecord::Schema.define(version: 20150204205926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20150204204359) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_token"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_token"], name: "index_sessions_on_session_token", unique: true, using: :btree
+  add_index "sessions", ["user_id"], name: "index_sessions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
